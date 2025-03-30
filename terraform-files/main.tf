6aade2c765a886a2dd36b693fe1d5ebf2e46ca19
shoaibops <137149864+shoaibops@users.noreply.github.com>
2025-03-30 16:21:08 +0530

    Update main.tf

--- terraform-files/main.tf
+++ terraform-files/main.tf
@@ -1,24 +1,31 @@
 resource "aws_instance" "test-server" {
-  ami = "ami-0e35ddab05955cf57"
-  instance_type = "t3.medium"
-  key_name = "aws-key"
+  ami             = "ami-0e35ddab05955cf57"
+  instance_type   = "t3.medium"
+  key_name        = "aws-key"
   vpc_security_group_ids = ["sg-06b4c17cdf01af9b0"]
+
   connection {
-     type = "ssh"
-     user = "ec2-user"
-     private_key = file("./aws-key.pem")
-     host = self.public_ip
-     }
+    type        = "ssh"
+    user        = "ec2-user"
+    private_key = file("./aws-key.pem")
+    host        = self.public_ip
+    timeout     = "2m"
+    agent       = false
+  }
+
   provisioner "remote-exec" {
-     inline = ["echo 'wait to start the instance' "]
+    inline = ["echo 'wait to start the instance'"]
   }
+
   tags = {
-     Name = "test-server"
-     }
-  provisioner "local-exec" {
-     command = "echo ${aws_instance.test-server.public_ip} > inventory"
-     }
-  provisioner "local-exec" {
-     command = "ansible-playbook /var/lib/jenkins/workspace/bankingproject/terraform-files/ansibleplaybook.yml"
-     }
+    Name = "test-server"
   }
+
+  provisioner "local-exec" {
+    command = "echo '[test-server]' > inventory && echo '${aws_instance.test-server.public_ip}' >> inventory"
+  }
+
+  provisioner "local-exec" {
+    command = "ansible-playbook -i inventory /var/lib/jenkins/workspace/bankingproject/terraform-files/ansibleplaybook.yml"
+  }
+}
